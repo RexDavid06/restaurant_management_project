694f3e4d67e795b908e1a935df34cb9888a69de3
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-08-09 13:56:20 +0100

    28183

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from home.models import Restaurant
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+
 
 # Create your views here.
-class HomeView(TemplateView):
+class HomeView(CreateView):
     model = Restaurant.objects.all()
     template_name = 'home/home.html'
     context_object_name = 'restuarant'
@@ -12,3 +14,5 @@ class RestaurantView(TemplateView):
     template_name = 'home/restaurant.html'
     
 
+class MenuItemView(CreateView):
+    template_name = 'home/menuItem.html'
