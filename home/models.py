82e164e332dbb8e7810e7000a7da79ce8b2cc3a2
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-08-09 13:50:37 +0100

    28186

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Restuarant(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField()
+    phone_number = models.CharField(max_length=17)
 
     def __str__(self):
         return self.name
