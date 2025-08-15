0940516f634b77fcbf43e091065eb7c83dc21780
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-08-15 11:13:43 +0100

    33509

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_item = models.ForeignKey(Item, on_delete.models.CASCADE)
+    total_amount = models.CharField(max_length=100)
+    order_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+        
