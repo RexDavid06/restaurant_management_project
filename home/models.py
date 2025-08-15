8d33652de83c50a4cbedc9311749483ec753dcab
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-08-15 11:44:58 +0100

    33529

--- home/models.py
+++ home/models.py
@@ -13,4 +13,8 @@ class Restuarant(models.Model):
     def __str__(self):
         return self.name
 
-    
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(amx_digits=1000, decimal_places=2)
