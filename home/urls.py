cf837da3b57e6cce0c35bea1503ac0a16ad54ca2
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-08-15 11:34:39 +0100

    33511

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('restaurant/', views.RestaurantView.as_view(), name='restaurant'),
     path('menuItem/', views.MenuItemView.as_view(), name='menuItem'),
+    path("restuarant_info/<int:pk>/", views.restuarant_info, name='restuarant_info')
 ]
