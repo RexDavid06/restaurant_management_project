34f9c3e281172f1ef56731f6cce3268f3c39bf72
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-08-15 11:16:37 +0100

    33507

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(User):
+    name = models.CharField(max_length=255)
+    email = models.Email.Field()
+    phone_number = models.CharField(max_length=17)
+
+    def __str__(self):
+        return self.name
