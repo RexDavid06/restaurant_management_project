c8efa917e5e8c30e5da6dc2a35249036b66b40fb
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-08-15 11:49:52 +0100

    33536

--- home/models.py
+++ home/models.py
@@ -19,3 +19,10 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     price = models.DecimalField(amx_digits=1000, decimal_places=2)
+
+
+class RestuarantLocation(models.Model):
+    address = models.CharField(max_length=400)
+    city = models.CharField(max_length=300)
+    state = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=10)
